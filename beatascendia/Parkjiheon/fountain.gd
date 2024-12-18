009b2d46074ce04fa33053a14ad73c65c8a50cc4
LakkeTree <parkjoyjoy@ajou.ac.kr>
2024-12-18 16:37:55 +0900

    .
    .

--- beatascendia/Parkjiheon/fountain.gd
+++ beatascendia/Parkjiheon/fountain.gd
@@ -9,4 +9,5 @@ func _on_body_entered(body: Node2D) -> void:
 	fin.visible = true
 	await get_tree().create_timer(3.0).timeout
 	fin.visible = false
-	get_tree().change_scene_to_file("res://Parkjiheon/Background2.tscn")
+	#get_tree().change_scene_to_file("res://Parkjiheon/Background2.tscn")
+	get_tree().change_scene_to_packed(preload("res://Parkjiheon/Background2.tscn"))
